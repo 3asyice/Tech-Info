f3582ad9a3ec5d6eff2d724c6d6d0d2f2bda084f
3asyice <guilhermeabreu294@gmail.com>
2025-10-01 00:42:56 -0300

    update

---
+++ script/js/2827x636372.js
@@ -0,0 +1,26 @@
+var x = document.getElementById('x');
+var y = false;
+
+function menu() {
+  y = !y;
+  x.style.display = y ?
+    'block' : 'none';
+}
+window.addEventListener("DOMContentLoaded", () => {
+  if (sessionStorage.getItem("logado") !== "true") {
+    
+    setTimeout(() => {
+      window.location.href =
+        "https://3asyice.github.io/SixOhNine/script/login.html?redirect=" + encodeURIComponent(window.location.href);
+    }, 500);
+  } else {
+    
+    document.body.classList.add("ready");
+  }
+});
+
+document.addEventListener("contextmenu", function(e) {
+  if (e.target.tagName === "IMG") {
+    e.preventDefault();
+  }
+});
